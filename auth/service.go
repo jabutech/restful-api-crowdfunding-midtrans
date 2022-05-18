f53c55b6a98fc65a057c23abfff20c8cf2ea2805
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-05-18 10:52:45 +0700

    feature:jwt auth, git validate

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,14 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(encodedToken string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -33,4 +38,24 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 
 	// If no error, return signedToken
 	return signedToken, nil
+}
+
+// Function service for Validate token
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	// Parse token
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
